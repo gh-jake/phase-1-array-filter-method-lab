3c3f35a38f677d46ef49552b9b15a358e10b826d
Jake Houston <jakechouston@gmail.com>
2021-03-17 19:05:25 -0400

    commit tree 1b8213034cdad586a71f93b452eb90a386d84c98

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, firstLetters) {
+    return drivers.filter(driver => driver.startsWith(firstLetters));
+}
+
+function matchName(drivers, searchedName) {
+    return drivers.filter(driver => driver.name === searchedName);
+}
